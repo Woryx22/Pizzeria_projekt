a83385b77383b12f422487aefca56f0660c31da9
Lukáš Lešák <lukas.kalna@seznam.cz>
2023-06-11 11:07:41 +0200

    vera

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 /***** Theme change *****/
 const btn = document.querySelector(".btn-toggle");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const themePreference = localStorage.getItem("themePreference");
+
+// Nastavení theme preference na základě local storage nebo system preference
+if (themePreference === "dark" || (themePreference === null && prefersDarkScheme.matches)) {
+  document.body.classList.add("dark-theme");
+} else {
+  document.body.classList.add("light-theme");
+}
 
 btn.addEventListener("click", function () {
   if (prefersDarkScheme.matches) {
     document.body.classList.toggle("light-theme");
-    
+    document.body.classList.toggle("dark-theme");
+    localStorage.setItem("themePreference", document.body.classList.contains("dark-theme") ? "dark" : "light");
   } else {
     document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("light-theme");
+    localStorage.setItem("themePreference", document.body.classList.contains("light-theme") ? "light" : "dark");
   }
 });
 
@@ -42,7 +53,7 @@ function closeNav() {
   document.getElementById("closeDivDark").style.width ="0";
   document.getElementById("welcome").style.display = "block";
 };
-
+/***** scroll to na menu.html *****/
 function scrolltoa(){
   var scrollDiv = document.getElementById("js-Appetizers").offsetTop;
   window.scrollTo({ top: scrollDiv-80, behavior: 'smooth'});
@@ -79,5 +90,55 @@ function scrolltoi(){
   var scrollDiv = document.getElementById("js-Dezerts").offsetTop;
   window.scrollTo({ top: scrollDiv-80, behavior: 'smooth'});
 }
+
+
+/***** locations preklikavani *****/
+document.getElementById("js-select-1").style.backgroundColor = "#aaaaaa";
+function section1() {
+  document.getElementById("js-sec1").style.display = "grid";
+  document.getElementById("js-select-1").style.backgroundColor = "#aaaaaa";
+  document.getElementById("js-select-2").style.backgroundColor = "#ecc122";
+  document.getElementById("js-select-3").style.backgroundColor = "#ecc122";
+  document.getElementById("js-sec2").style.display = "none";
+  document.getElementById("js-sec3").style.display = "none";
+
+  // Uložení stavu vybrané sekce do localStorage
+  localStorage.setItem("selectedSection", "section1");
+}
+
+function section2() {
+  document.getElementById("js-sec1").style.display = "none";
+  document.getElementById("js-sec2").style.display = "grid";
+  document.getElementById("js-select-1").style.backgroundColor = "#ecc122";
+  document.getElementById("js-select-2").style.backgroundColor = "#aaaaaa";
+  document.getElementById("js-select-3").style.backgroundColor = "#ecc122";
+  document.getElementById("js-sec3").style.display = "none";
+
+  localStorage.setItem("selectedSection", "section2");
+}
+
+function section3() {
+  document.getElementById("js-sec1").style.display = "none";
+  document.getElementById("js-sec2").style.display = "none";
+  document.getElementById("js-sec3").style.display = "grid";
+  document.getElementById("js-select-1").style.backgroundColor = "#ecc122";
+  document.getElementById("js-select-2").style.backgroundColor = "#ecc122";
+  document.getElementById("js-select-3").style.backgroundColor = "#aaaaaa";
+
+  localStorage.setItem("selectedSection", "section3");
+}
+
+// Obnovení vybrané sekce při načtení stránky
+window.onload = function () {
+  var selectedSection = localStorage.getItem("selectedSection");
+  if (selectedSection === "section1") {
+    section1();
+  } else if (selectedSection === "section2") {
+    section2();
+  } else if (selectedSection === "section3") {
+    section3();
+  }
+};
+
 
 
